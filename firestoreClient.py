33a370d36cb01c1e13df9e3b1f9a552ebebdabf4
John Schiele <jschiele@s1med.onmicrosoft.com>
2023-05-09 11:53:31 -0400

    firestore JSON config file

--- firestoreClient.py
+++ firestoreClient.py
@@ -4,8 +4,7 @@ from firebase_admin import credentials, firestore
 
 # function created to establish a connection to the Firestore Database using JSON credentials file
 def client_setup():
-    cred = credentials.Certificate("C:/Users/jpsch/OneDrive/Documents/SeniorProject/"
-                                   "schieleproject-firebase-adminsdk-m7rjq-33cb8281fc.json")
+    cred = credentials.Certificate("schieleproject-firebase-adminsdk-m7rjq-33cb8281fc.json")
     try:
         firebase_admin.get_app()
     except ValueError:
